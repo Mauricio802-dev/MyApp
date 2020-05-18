2b6de259a6b810ee7a0ebe0ec0d09b6bc09fa839
Mauricio Sanchez <dsmauricio16@gmail.com>
2020-05-17 22:42:49 -0400

    listo_para usar

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var login = function()
+{
+    var email = document.getElementById("email").value;
+    var passw = document.getElementById("passw").value;
+
+    console.log("Loggin");
+    console.log(`Usuario: ${email}`);
+    console.log(`Contraseña: ${passw}`);
+}
